2db04d19a5087bce1ed67a8ab466f61aff08e18b
Douglas Nomizo <douglasnomizo@outlook.com>
2016-12-06 20:46:29 -0200

    Clean up

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -5,17 +5,17 @@ class ItemTest < ActiveSupport::TestCase
     @item = items(:water)
   end
 
-  test "item should save correctly" do
+  test "should save correctly" do
     @item = Item.new name: "Food", points: 2
     assert @item.save
   end
 
-  test "item should have name" do
+  test "should have name" do
     @item.name = nil
     assert_not @item.save
   end
 
-  test "item should have positive points" do
+  test "should have positive points" do
     @item.points = -1
     assert_not @item.save
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,17 +5,17 @@ class UserTest < ActiveSupport::TestCase
     @user = users(:carol)
   end
 
-  test "user should have name" do
+  test "should have name" do
     @user.name = nil
     assert_not @user.save
   end
 
-  test "user should have gender" do
+  test "should have gender" do
     @user.gender = nil
     assert_not @user.save
   end
 
-  test "age should be greater than zero" do
+  test "should have age greater than zero" do
     @user.age = 0
     assert_not @user.save
   end
